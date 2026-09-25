dd4028bc012e2846983f84090ed4120b3211f033
CreateSource <abnormalhare822@gmail.com>
2026-09-25 15:53:14 -0400

    start work on srender

---
+++ src/Speed/Indep/Libs/snd/9/source/library/cmn/srender.c
@@ -0,0 +1,7 @@
+#include "Speed/Indep/Libs/snd/9/source/library/cmn/sndcmn.h"
+
+int SNDI_validrendermode(int *prenderindex, SNDIPATCHHEADER *pph) {
+    int validrendermode;
+    int globalplayloc;
+    int globalspatialization;
+}
